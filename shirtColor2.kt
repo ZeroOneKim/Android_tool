a41887499c1bb31ebe3294888b402dd1170c3f4e
ZeroOneKim <83469080+ZeroOneKim@users.noreply.github.com>
2021-07-31 12:42:00 +0900

    upgrade
    shirtcolor => only camera app
    shritcolor2 => add a Gallery function

---
+++ shirtColor2.kt
@@ -0,0 +1,157 @@
+package com.example.coloris
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+
+class shirtColor : AppCompatActivity() {
+
+    val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+    val STORAGE_PERMISSION = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+    val FLAG_PERM_CAMERA = 98
+    val FLAG_PERM_STORAGE = 99
+
+    val FLAG_REQ_CAMERA = 101
+    val FLAG_REQ_GALLERY = 102 //
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shirt_color)
+
+        val buttonCamera = findViewById(R.id.buttonCamera) as Button
+        buttonCamera.setOnClickListener {
+            if(isPermitted(CAMERA_PERMISSION)) {
+                openCamera()
+            } else {
+                ActivityCompat.requestPermissions(this, CAMERA_PERMISSION, FLAG_PERM_CAMERA)
+            }
+        }
+        val buttonGallery = findViewById(R.id.buttonGallery) as Button
+        buttonGallery.setOnClickListener {
+            if(isPermitted(STORAGE_PERMISSION)) {
+                openGallery()
+            } else {
+                ActivityCompat.requestPermissions(this, STORAGE_PERMISSION, FLAG_PERM_STORAGE)
+            }
+        }
+
+    }
+
+    fun isPermitted(permissions: Array<String>): Boolean {
+
+        for (permission in permissions) {
+            val result = ContextCompat.checkSelfPermission(this, permission)
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+    fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, FLAG_REQ_CAMERA)  //카메라 찍으면 그대로 나한테 준다
+    }
+    fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = MediaStore.Images.Media.CONTENT_TYPE
+        startActivityForResult(intent, FLAG_REQ_GALLERY)
+    }
+
+    //camera save1
+    fun saveImageFile(filename:String, mimeType:String, bitmap:Bitmap) : Uri? {
+        var values = ContentValues()
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, filename)
+        values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.IS_PENDING, 1)
+        }
+
+        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values) //저장할 사진의 주소값 생성
+
+        try {
+            if (uri != null) {
+                var descriptor = contentResolver.openFileDescriptor(uri, "w")
+
+                if (descriptor != null) {
+                    val fos = FileOutputStream(descriptor.fileDescriptor)
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+                    fos.close()
+                    return uri
+                }
+            }
+        } catch(e:Exception) {
+            Log.e("Camera", "${e.localizedMessage}")
+        }
+        return null
+    }
+    //GS2
+    fun newFileName() : String {
+        val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
+        val filename = sdf.format(System.currentTimeMillis())
+        return filename
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                FLAG_REQ_CAMERA -> {
+                    if (data?.extras?.get("data") != null) { //GS3
+                        val bitmap = data?.extras?.get("data") as Bitmap
+
+                        //val filename = newFileName()  //GS4
+                        //val uri = saveImageFile(filename, "iamge/jpg", bitmap)
+
+                        val imagePreview = findViewById<View>(R.id.imagePreview) as ImageView
+                        imagePreview.setImageBitmap(bitmap)  //GS5
+                    }
+                }
+                FLAG_REQ_GALLERY -> {
+                    val uri = data?.data
+                    val imagePreview = findViewById<View>(R.id.imagePreview) as ImageView
+                    imagePreview.setImageURI(uri)//
+                }
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            FLAG_PERM_CAMERA -> {
+                var checked = true
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        checked = false
+                        break
+                    }
+                }
+                if (checked) {
+                    openCamera()
+                }
+            }
+        }
+    }
+}
